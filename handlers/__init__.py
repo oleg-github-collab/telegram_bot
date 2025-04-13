99dab17b0f2e08297cc83d2bc248d6c71b841fd5
Oleh Kaminskyi <work.olegkaminskyi@gmail.com>
2025-04-13 20:34:15 +0200

    www

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,4 @@
-
+from .user import start, help_command, get_content, button_callback
 from .admin import (
     admin_panel, admin_button_callback, title_handler, description_handler,
     content_handler, confirmation_handler, delete_content_callback,
